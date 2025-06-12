2b7790dce5d188d7f1e54f43fff927f33f76272e
Matias <matiasmantaras220603@gmail.com>
2025-06-11 21:03:04 -0300

    Actualizacion del backend en para el detalles del dispositivo y para nuevos dispositivos

--- Backend.py
+++ Backend.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configuración de la base de datos PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:110402BES44150784@localhost:5432/network_backup'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:44987813@localhost:5432/network_backup'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicializar SQLAlchemy
@@ -17,6 +17,16 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+class Dispositivo(db.Model):
+    
+    nombre = db.Column(db.String(100), nullable=False)
+    ip = db.Column(db.String(50), nullable=False)
+    Tipo = db.Column(db.String(50), nullable=False)
+    Usuario = db.Column(db.String(50), nullable=False)
+    contrasena = db.Column(db.String(100), nullable=False)
+    puerto_ssh = db.Column(db..Integer, nulleable=False)
+
 
 @app.route('/login', methods=['POST'])
 def login():        
@@ -30,6 +40,33 @@ def login():
         return jsonify({"success": True, "message": f"Bienvenido, {username}!"})
     else:
         return jsonify({"success": False, "message": "Usuario o contraseña incorrectos."}), 401
+
+@app.route('/dispositivo', methods=['POST'])
+def agregar_dispositivo():
+    data = request.json
+    
+    nombre = data.get('nombre')
+    ip = data.get('ip')
+    tipo = data.get('tipo')
+    usuario = data.get('usuario')
+    contrasena = data.get('contrasena')
+    ssh = data.get('ssh')
+    
+
+    
+    nuevo_dispositivo = Dispositivo(
+        nombre=nombre,
+        ip=ip,
+        tipo=tipo,
+        usuario=usuario,
+        contrasena=contrasena,
+        ssh=ssh
+        
+    )
+    db.session.add(nuevo_dispositivo)
+    db.session.commit()
+
+    return jsonify({"success": True, "message": "Dispositivo agregado correctamente"})
 
 if __name__ == '__main__':
     # Crea las tablas si no existen (solo la primera vez)
